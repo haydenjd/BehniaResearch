b07c945df928ea611e4b7f2298b353e05c466d91
Jimmy <haydenjd@clarkson.edu>
2019-11-07 16:37:50 -0500

    Updated code for 11/7 meeting

--- code/test.py
+++ code/test.py
@@ -1,31 +1,55 @@
 import cv2
 import numpy as np
 
+def thinning(name):
+    img = cv2.imread(name,0)
+    size = np.size(img)
+    skel = np.zeros(img.shape,np.uint8)
+     
+    ret,img = cv2.threshold(img,127,255,0)
+    element = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+    done = False
+     
+    while( not done):
+        eroded = cv2.erode(img,element)
+        temp = cv2.dilate(eroded,element)
+        temp = cv2.subtract(img,temp)
+        skel = cv2.bitwise_or(skel,temp)
+        img = eroded.copy()
+     
+        zeros = size - cv2.countNonZero(img)
+        if zeros==size:
+            done = True
+     
+    cv2.imshow("skel",skel)
+    cv2.waitKey(0)
+    #cv2.destroyAllWindows()
+
 def select(file1):
     image = cv2.imread('file1.jpg')
     blur = cv2.medianBlur(image, 7)
     gray = cv2.cvtColor(blur, cv2.COLOR_BGR2GRAY)
     thresh = cv2.adaptiveThreshold(gray,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,3)
-    
+
     canny = cv2.Canny(thresh, 120, 255, 1)
     kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
     opening = cv2.morphologyEx(canny, cv2.MORPH_CLOSE, kernel)
     dilate = cv2.dilate(opening, kernel, iterations=2)
-    
+
     cnts = cv2.findContours(dilate, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-    
+
     min_area = 3000
     for c in cnts:
         area = cv2.contourArea(c)
         if area > min_area:
             cv2.drawContours(image, [c], -1, (36, 255, 12), 2)
-    
+
     cv2.imshow('image', image)
     cv2.imwrite('image.png', image)
     cv2.waitKey(0)
 
-######################################## MAIN FUNCTION #################################################
+#############################################MAIN####################################################
 #image = cv2.resize((cv2.imread('crack1.jpg')), (1500,1500))
 img = cv2.imread('crack1.jpg')
 cv2.imwrite("file2.jpg", cv2.resize(img, (1500,1500)))
@@ -92,10 +116,10 @@ if input_img is not None:
                     #pixels("file.jpg")
                     #display()
                     select("file1.jpg")
+                    thinning("file.jpg")
                     cv2.destroyAllWindows()
                     break
 
 else:
         print("Please Check The Path of Input File")
-
 
