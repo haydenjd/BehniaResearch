5a5ff2db67f4d36c69b9caf2edce177367c92b5b
Jimmy <haydenjd@clarkson.edu>
2019-11-13 10:36:41 -0500

    Update

--- code/test.py
+++ code/test.py
@@ -1,5 +1,8 @@
 import cv2
 import numpy as np
+from skimage import img_as_float
+from skimage import io, color, morphology
+import matplotlib.pyplot as plt
 
 def thinning(name):
     img = cv2.imread(name,0)
@@ -24,6 +27,28 @@ def thinning(name):
     cv2.imshow("skel",skel)
     cv2.waitKey(0)
     #cv2.destroyAllWindows()
+
+def thinning2(name):
+    image = img_as_float(color.rgb2gray(io.imread('line.png')))
+    image_binary = image < 0.5
+    out_skeletonize = morphology.skeletonize(image_binary)
+    out_thin = morphology.thin(image_binary)
+    
+    
+    f, (ax0, ax1, ax2) = plt.subplots(1, 3, figsize=(10, 3))
+    
+    ax0.imshow(image, cmap='gray')
+    ax0.set_title('Input')
+    
+    ax1.imshow(out_skeletonize, cmap='gray')
+    ax1.set_title('Skeletonize')
+    
+    ax2.imshow(out_thin, cmap='gray')
+    ax2.set_title('Thin')
+    
+    plt.savefig('newFIg.jpg')
+    plt.show()
+
 
 def select(file1):
     image = cv2.imread('file1.jpg')
